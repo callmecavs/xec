6b53283ab0e027ea78d38d783b5b35c51ced05cf
Michael Cavalea <callmecavs@gmail.com>
2016-08-12 18:08:24 -0400

    add proto single script loading

--- src/xecute.js
+++ src/xecute.js
@@ -1,3 +1,26 @@
-export default () => {
+const xecute = source => new Promise((resolve, reject) => {
+  // create the script tag
+  const tag = document.createElement('script')
 
-}
+  // remove event listeners, resolve or reject the Promise
+  const done = func => {
+    tag.onerror = null
+    tag.onload = null
+    func()
+  }
+
+  // add error and load event listeners
+  tag.onerror = done(reject)
+  tag.onload = done(resolve)
+
+  // set async to true
+  tag.async = true
+
+  // set the source
+  tag.src = source
+
+  // append the script tag to the DOM
+  document.body.appendChild(tag)
+})
+
+export default xecute
