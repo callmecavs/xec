2e26e660526133510bab41ed809737c35250bb82
callmecavs <callmecavs@gmail.com>
2017-01-08 17:17:56 -0500

    remove gulp config, add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
+const info = require('./package.json')
+
+const config = {
+  entry: 'src/xec.js',
+  plugins: [
+    resolve(),
+    babel()
+  ],
+  targets: [
+    {
+      dest: info.main,
+      format: 'umd',
+      moduleName: 'Xec'
+    }, {
+      dest: info.module,
+      format: 'es'
+    }
+  ]
+}
+
+export default config
